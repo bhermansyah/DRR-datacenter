833d5f5eae685462628d774cd566748be995fa1d
budi <budi@budis-MacBook-Pro.local>
2015-12-13 16:01:33 +0430

    ......

--- geonode/static/lib/js/GeoExplorer.js
+++ geonode/static/lib/js/GeoExplorer.js
@@ -35000,7 +35000,9 @@ GeoExt.data.PrintProvider = Ext.extend(Ext.util.Observable, {
                         var params = Ext.urlDecode(split[1]);
                         console.log(params)
                         params['SCALE'] = scale;
-                        params['legend_options'] = 'fontSize:8'
+                        params['legend_options'] = 'fontAntiAliasing:true;fontSize:9;fontName:Dialog.plain';
+                        params['width'] = 15;
+                        params['height'] = 15;
                         url = split[0] + "?" + Ext.urlEncode(params);
                     }
                     icons.push(this.getAbsoluteUrl(url));
