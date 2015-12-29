b1021c3214c94631a4036ee0b37eb87450307151
budi <budi@budis-MacBook-Pro.local>
2015-12-29 16:45:44 +0430

    ...

--- geonode/base/models.py
+++ geonode/base/models.py
@@ -746,5 +746,9 @@ class matrix(models.Model):
     user = models.ForeignKey(get_user_model())
     resourceid = models.ForeignKey(ResourceBase)
     action = models.CharField(max_length=255, help_text='for example "download,view"')
+
+    def __unicode__(self):
+        return self.action
+
    
 
