23ea666f9fdf0a31abc53b4ba86ac17d857b53db
budi <budi@budis-MacBook-Pro.local>
2015-12-30 11:55:11 +0430

    ....

--- matrix/models.py
+++ matrix/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from geonode.base.models import ResourceBase
 from django.contrib.auth import get_user_model
+import datetime
 
 class matrix(models.Model):
     user = models.ForeignKey(get_user_model())
     resourceid = models.ForeignKey(ResourceBase)
     action = models.CharField(max_length=255, help_text='for example "download,view"')
-
+    created     = models.DateTimeField(editable=False)
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = datetime.datetime.now()
+        return super(matrix, self).save(*args, **kwargs)
     def __unicode__(self):
         return self.action
