17232cd3f557470368f5aac2da0b99831be401aa
budi <budi@budis-MacBook-Pro.local>
2016-02-26 15:54:45 +0430

    ....

--- geodb/tasks.py
+++ geodb/tasks.py
@@ -3,7 +3,7 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 from datetime import datetime
-from geodb.views import getForecastedDisaster, updateSummaryTable
+from geodb.views import getForecastedDisaster, updateSummaryTable, getSnowCover
 
 logger = get_task_logger(__name__)
 
@@ -14,4 +14,5 @@ def scraper_example():
 @periodic_task(run_every=(crontab(hour='*')))
 def updateDistrictsSummary():
 	updateSummaryTable()
+	getSnowCover()
 
