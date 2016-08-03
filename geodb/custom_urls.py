2e2948ae7c2df6ffcffe1763fd53559198a2f50f
budi <budi@budis-MacBook-Pro.local>
2016-08-03 15:00:58 +0700

    ...

--- geodb/custom_urls.py
+++ geodb/custom_urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns(
     url(r'^snowinfo$', 'getSnowVillage', name='getSnowVillage'),
     url(r'^accessibilityinfo$', 'getAccesibilityInfoVillages', name='getAccesibilityInfoVillages'),
     url(r'^floodinfo$', 'getFloodInfoVillages', name='getFloodInfoVillages'),
+    url(r'^earthquakeinfo$', 'getEarthquakeInfoVillages', name='getEarthquakeInfoVillages'),    
     url(r'^getWMS$', 'getWMS', name='getWMS'),
 )
