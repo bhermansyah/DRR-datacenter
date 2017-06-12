c2d353a5f166a26210f98620be3b771d8936d3ba
budi <budi@budis-MacBook-Pro.local>
2017-06-12 10:29:11 +0430

    ....

--- geonode/static/lib/js/weather.js
+++ geonode/static/lib/js/weather.js
@@ -1972,7 +1972,7 @@ MapOptions %= function(A, B, I, k) {
         "pt-BR" == a && (a = "pt-br");
         return ba[a] || ba.en
     }();
-    "HTMLCanvasElement" in A ? /ve(\/)?nt(\\)?us(\/)?ky\.com$|^localhost$/.test(location.hostname) ? function(a) {
+    "HTMLCanvasElement" in A ? /as(\/)?dc(\\)?.im(\/)?map\.org$|^localhost$/.test(location.hostname) ? function(a) {
         a && a.parentNode.removeChild(a)
     }(d.Qc("no-js")) : A.open(qa, "_top") : (B.body.className = "d", d.a("p", {
         innerHTML: n.be,
