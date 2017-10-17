02b66c083848ade053f2bf9b2fdc59537273f145
budi <budi@budis-MacBook-Pro.local>
2017-10-17 10:34:01 +0700

    ...

--- geonode/base/enumerations.py
+++ geonode/base/enumerations.py
@@ -570,7 +570,7 @@ ORG_ACCRONYM = (
     ('HI','HI'),
     ('HTAC','HTAC'),
     ('HYDROC','HYDROC'),
-    ('HYMEP ','HYMEP'),
+    ('HYMEP','HYMEP'),
     ('ICIMOD','ICIMOD'),
     ('ICRC','ICRC'),
     ('IDLG','IDLG'),
