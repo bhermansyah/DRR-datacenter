d332b7bcaebbb85814da265422c5adad9a9a0539
Rafinkanisa <rafinkanisaw@gmail.com>
2018-04-10 14:27:54 +0700

    revert back

--- geonode/static/v2/js/custom.js
+++ geonode/static/v2/js/custom.js
@@ -1162,7 +1162,7 @@ jQuery(function($){
 				if (dir_read=='rtl') {
 					return {renderer: 'canvas'};
 				}else {
-					return {renderer: 'svg'};
+					return {renderer: 'canvas'};
 				};
 			}
 
