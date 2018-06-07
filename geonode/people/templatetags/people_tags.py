bf6bdddcc827666b1b10a8dd8e51e8d514e6357e
sud0su <ahmat.raz@gmail.com>
2018-06-07 15:25:52 +0700

    Groups page based on Organization

--- geonode/people/templatetags/people_tags.py
+++ geonode/people/templatetags/people_tags.py
@@ -6,9 +6,13 @@ register = template.Library()
 
 @register.filter(name='file_exists')
 def file_exists(filepath):
-    default_path = 'geonode'+filepath
-    if os.path.exists(default_path):
-        return filepath
+    # default_path = 'geonode'+filepath
+    default_path = 'home/ubuntu/DRR-datacenter/geonode'+filepath #serverside
+    print default_path
+    if os.path.exists(default_path+'.png'):
+        return filepath+'.png'
+    elif os.path.exists(default_path+'.jpg'):
+        return filepath+'.jpg'
     else:   
         no_image_path =  settings.STATIC_URL+'v2/images/nologo.png'
         return no_image_path
