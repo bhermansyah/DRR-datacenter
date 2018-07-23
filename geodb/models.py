fbcb2e9ba58c8fc378f326ffd55af056b69594f2
Budi Hermansyah <bhermansyah@immap.org>
2018-07-23 11:41:49 +0700

    initial for drought calculation

--- geodb/models.py
+++ geodb/models.py
@@ -1921,3 +1921,19 @@ class EventdataHistory(models.Model):
     class Meta:
         managed = False
         db_table = 'eventdata_history'
+
+class HistoryDrought(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    ogc_fid = models.IntegerField(blank=True, null=True)
+    min = models.FloatField(blank=True, null=True)
+    mean = models.FloatField(blank=True, null=True)
+    max = models.FloatField(blank=True, null=True)
+    std = models.FloatField(blank=True, null=True)
+    sum = models.FloatField(blank=True, null=True)
+    count = models.FloatField(blank=True, null=True)
+    basin_id = models.FloatField(blank=True, null=True)
+    agg_code = models.CharField(max_length=50, blank=True)
+    woy = models.CharField(max_length=-1, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'history_drought'
